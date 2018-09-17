b32c71f429806e181d048a7f1a0d7c4c934969b3
Stevenson <rstevenson@usgs.gov>
2018-09-17 08:39:43 -0600

    Updated Colors for TP  S5 and S7

--- src/scripts/sparrow-config.js
+++ src/scripts/sparrow-config.js
@@ -141,7 +141,7 @@ var nitrogenSourceDefinitions = {
     1. each color corresponds with the order of SourceDefinitions objects above  
     2. there the number of hex colors matches the number of nutrient sources
 **/
-var phosColors = ["#BF0000", "#FFCCFF", "#663100", "#FFEC99", "#A2EB85", "#0070C0"];
+var phosColors = ["#BF0000", "#FFCCFF", "#663100", "#FFEC99", "#EAA720", "#0070C0","#00a900"];
 var nitroColors = ["#BF0000", "#FFCCFF", "#663100", "#FFEC99", "#A2EB85", "#00a900", "#006800", "#0070C0"];
 
 /***-----BEGIN PHOSPHORUS LAYER GROUPS --------***/
